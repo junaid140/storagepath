4b87fc10466083c581d2d3893c609e9038417437
junaid140 <50622505+junaid140@users.noreply.github.com>
2022-09-15 11:26:31 +0500

    first commit

---
+++ example/android/app/src/main/java/com/follow2vivek/storagepathexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.follow2vivek.storagepathexample;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+public class MainActivity extends FlutterActivity {
+//  @Override
+//  protected void onCreate(Bundle savedInstanceState) {
+//    super.onCreate(savedInstanceState);
+//    GeneratedPluginRegistrant.registerWith(this);
+//  }
+}
---
+++ example/lib/view/home.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import '../tabview/audio.dart';
+import '../tabview/file.dart';
+import '../tabview/image.dart';
+import '../tabview/video.dart';
+
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Color(0xffe67e22),
+          title: Text(
+            'Gallery',
+            style: TextStyle(color: Colors.white, fontFamily: 'Bold'),
+          ),
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            indicatorSize: TabBarIndicatorSize.tab,
+            labelPadding: EdgeInsets.only(bottom: 15),
+            tabs: <Widget>[
+              Text(
+                'Images',
+                style: TextStyle(
+                    color: Colors.white, fontSize: 18, fontFamily: 'Medium'),
+              ),
+              Text(
+                'Videos',
+                style: TextStyle(
+                    color: Colors.white, fontSize: 18, fontFamily: 'Medium'),
+              ),
+              Text(
+                'Audios',
+                style: TextStyle(
+                    color: Colors.white, fontSize: 18, fontFamily: 'Medium'),
+              ),
+              Text(
+                'Files',
+                style: TextStyle(
+                    color: Colors.white, fontSize: 18, fontFamily: 'Medium'),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Images(),
+            Video(),
+            Audio(),
+            File(),
+          ],
+        ),
+      ),
+    );
+  }
+}
